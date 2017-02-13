8896fc42d4ca346568aef8a8eca8fa84077b4882
Hana Kim <hanakim@Hanas-MacBook-Pro-2.local>
2017-02-13 16:21:19 -0500

    not working

--- stickies.js
+++ stickies.js
@@ -1,92 +1,58 @@
-
-
-
-
-
-
 //a data structure for storing information about the team
 var stickyObject = {
     stickies: [
         {
-            text: "",
+            text: ,
         }
     ],
 }
 
 function createNote(){
     stickyObject.stickies.push({text:""})
+    localStorage.setItem(notes, stickyObject);
+    localStorage.setItem('notes', JSON.stringify(stickyObject));
     displayStickies();
 }
 
-
-
-// function createNote(notes) {
-//     // add a new note to the list
-//     notes.append("<li><div class='aNote'>" + 
-//                     "<a class='glyphicon glyphicon-remove' />" +
-//                     "<br>" +
-//                     "<textarea class='note-content' placeholder='Your note'/>" +  
-//                     "</div></li>"); 
-
-//     // Close function
-//     $(".glyphicon-remove").click(function () {
-//         console.log($('this').closest('li'));
-//         $(this).closest('li').remove();
-//     });
-
-//     }
-
-//  $(document).ready(function () {
-//     // get references to the 'notes' list
-//     notes = $("#stickies");
-
-
-//     // clicking the 'New Note' button adds a new note to the list
-//     $("#newButton").click(function () {
-//         createNote(notes);
-//     });
-
-// });
-
 function removeNote(){
    stickyObject.stickies.splice(event.target.getAttribute("data-index"),1);
+   localStorage.setItem('notes', JSON.stringify(stickyObject));
    displayStickies(); 
 }
 
-function displayStickies() { //creates the html elements to show the team
-    // $("#teamName").html(team.teamName); //add the team name to the appropriate div
-    // TODO 2: document lines 34-43 such that anyone reading your comment will understand what this code does
+function saveNotes(){
+    console.log("saved");
+    var parsedStickies = JSON.parse(localStorage.getItem('notes'));
+
+
+    $('textarea').each(function(i, e) {
+        console.log(e);  
+        // 0: <div id="outer"><div id="inner"></div></div>
+        // 1: <div id="inner"></div>
+    });
+
+    // for (var i=0; i< parsedStickies.stickies.length; i++) {
+    //     parsedStickies.
+    // }
+
+}
+
+function displayStickies() { 
     var stickiesHtml = "";
-    for (var i=0; i<stickyObject.stickies.length; i++) {
+    var parsedStickies = JSON.parse(localStorage.getItem('notes'));
+    for (var i=0; i< parsedStickies.stickies.length; i++) {
         stickiesHtml += "<a class='glyphicon glyphicon-remove removeButton' data-index='" + i + "' />" + "<br>";
-        stickiesHtml += '<textarea class="sticky">' + stickyObject.stickies[i].text + '</textarea>';      
+        // stickiesHtml += '<textarea class="sticky">' + stickyObject.stickies[i].text + '</textarea>';
+        stickiesHtml += '<textarea class="sticky">' + parsedStickies.stickies[i].text + '</textarea>';      
     }
 
     $("#stickiesList").empty().append(stickiesHtml);
 
-      // Close function
-      $(".removeButton").click(function () {
-            // stickyObject.stickies.splice(event.target.getAttribute("data-index"),1);
-            removeNote();
-        });
+    // Close function
+    $(".removeButton").click(function () {
+        removeNote();
+    });
 
-
-
-
-    // // an event triggered when the cursor hovers over a teammate div
-    // $(".aSticky").mouseenter(function(event) {
-    //     $(this).addClass("spotlight"); //adds a "spotlight" (yellow highlighting) to the teammate
-    // });
-
-    // // TODO 9: add an event that removes the spotlight from a teammate when the cursor leaves the teammate's div
-    // $(".aSticky").mouseleave(function(event) {
-    //     $(this).removeClass("spotlight"); //adds a "spotlight" (yellow highlighting) to the teammate
-    // });
-
-    // $(".aSticky").click(function() { // TODO 8: write a comment explaining what this function does
-    //     incrementPopularity($(this).attr("data-index"))
-
-    // });
 }
 
 
@@ -97,6 +63,10 @@ $(function() {
 
     $("#newButton").click(function () {
         createNote();
+    });
+
+    $("#saveButton").click(function () {
+    saveNotes();
     });
 
 });
